aff409d9956517c6c4e4469af143dc9fdaab7b75
Josh <j.esgar0027@gmail.com>
2020-01-28 14:10:28 -0600

    completed demo

--- src/test/java/com/pluralsight/controller/RestControllerTest.java
+++ src/test/java/com/pluralsight/controller/RestControllerTest.java
@@ -18,8 +18,8 @@ public class RestControllerTest {
 		RestTemplate restTemplate = new RestTemplate();
 		
 		Ride ride = new Ride();
-		ride.setName("Willow Trail Ride");
-		ride.setDuration(30);
+		ride.setName("Buzzsaw Falls Ride");
+		ride.setDuration(45);
 		
 		restTemplate.put("http://localhost:8080/ride_tracker/ride", ride);
 	}
